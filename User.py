e6d183afe0fe6ff956241b965242b5140dc7abe8
dmakger <dmakger@yandex.ru>
2021-11-26 00:44:46 +0300

    Добавить класс User для получения данных о пользователе

---
+++ User.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+from Url import Url
+
+
+class User:
+    URL_USER = "https://ies.unitech-mo.ru/user"
+    TEXT_SPECIALITY = "Специальность / направление подготовки:"
+    TEXT_GROUP = "Группа:"
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.soup = BeautifulSoup(Url.get_html(driver=self.driver, url=self.URL_USER), 'html.parser')
+
+    def get_corporate_data(self, param: str = None):
+        data = self.soup.find_all('div', class_="userpage_block_wrap")[1].find_all('div', class_="info")
+        if param is None:
+            corporate_data = list()
+            for info in data:
+                title = info.find("strong")
+                if title:
+                    corporate_data.append(info.get_text(strip=True).split(title.get_text(strip=True))[1])
+            return corporate_data
+
+        for info in data:
+            title = info.find("strong")
+            if title and (title.get_text(strip=True) == param):
+                return info.get_text(strip=True).split(title.get_text(strip=True))[1]
+        return None
+
